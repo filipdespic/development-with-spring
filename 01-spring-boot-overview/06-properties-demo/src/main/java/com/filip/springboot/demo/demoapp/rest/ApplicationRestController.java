fe7997c99f3f536ca57bf685d18f79fab60cd397
Filip Despic <filip.despic@sportserve.co>
2024-06-14 17:37:12 +0200

    add mini project that will test the custom application.properties

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/filip/springboot/demo/demoapp/rest/ApplicationRestController.java
@@ -0,0 +1,31 @@
+package com.filip.springboot.demo.demoapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationRestController {
+
+    // inject properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+}
