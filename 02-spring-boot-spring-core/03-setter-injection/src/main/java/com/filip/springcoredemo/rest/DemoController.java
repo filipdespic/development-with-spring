ef7ac469f9a2fafe92defc96ddc967885939a078
Filip Despic <filip.despic@sportserve.co>
2024-06-24 10:41:41 +0200

    add example of setter injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/filip/springcoredemo/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.filip.springcoredemo.rest;
+
+import com.filip.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyWorkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
