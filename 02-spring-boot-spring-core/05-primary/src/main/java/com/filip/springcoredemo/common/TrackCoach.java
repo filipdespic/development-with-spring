988cbd6dae4e4f2d99d3fcf6c5e688de3f973382
Filip Despic <filip.despic@sportserve.co>
2024-06-25 15:15:08 +0200

    add project for practicing @Primary annotation

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/filip/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,14 @@
+package com.filip.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class TrackCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run 500 meters.";
+    }
+}
