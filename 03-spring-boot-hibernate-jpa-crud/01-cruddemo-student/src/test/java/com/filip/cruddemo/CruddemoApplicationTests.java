bfcfb29a9be62082c7b4514d597a6c995a73158a
Filip Despic <filip.despic@sportserve.co>
2024-07-08 16:10:01 +0200

    add starter sql project and crud demo

---
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/test/java/com/filip/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
